641206e5e21c0a7c7e6935410edeaaa72cd3412a
asayee0 <asa.yee@hotmail.com>
2020-10-28 14:37:06 -0400

    #f4pny6 React works on dev server

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-document.getElementById("root").innerHTML = "<h1>Hello world!</h1>"
-// const e = React.createElement;
-// element = e('h1', null, 'Hello world');
-// ReactDOM.render(element, document.getElementById("root"));
+// document.getElementById("root").innerHTML = "<h1>Hello world!</h1>"
+const e = React.createElement;
+element = e('h1', null, 'Hello world!');
+ReactDOM.render(element, document.getElementById("root"));
