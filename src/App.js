985ee4d6ffd50c664500d9af1c306df5d369cd2d
asayee0 <asa.yee@hotmail.com>
2020-11-04 11:17:12 -0400

    System shows more information about item

--- src/App.js
+++ src/App.js
@@ -4,43 +4,88 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
+import Card from 'react-bootstrap/Card';
 
 function SetGoldModal(props) {
 	const [show, setShow] = useState(false);
 	const handleClose = () => setShow(false);
 	const handleShow = () => setShow(true);
 	const handleSubmit = (event) => {
-		console.log(props.gold);
 		event.preventDefault();
 	}
 	return (
 		<>
-			<Button variant="primary" onClick={handleShow}>
-				{props.gold} Gold
-			</Button>
+		<Button variant="primary" onClick={handleShow}>
+			{props.gold} Gold
+		</Button>
 
-			<Modal show={show} onHide={handleClose} backdrop={false}>
-				<Form onSubmit={handleSubmit}>
-					<InputGroup>
-						<Form.Control type="number" placeholder="Set gold amount" onChange={props.handleChange}/>
-						<Button type="submit" onClick={handleClose}>✓</Button>
-					</InputGroup>
-				</Form>
-			</Modal>
+		<Modal show={show} onHide={handleClose} backdrop={false}>
+			<Form onSubmit={handleSubmit}>
+				<InputGroup>
+					<Form.Control type="number" placeholder="Set gold amount" onChange={props.handleChange}/>
+					<Button type="submit" onClick={handleClose}>✓</Button>
+				</InputGroup>
+			</Form>
+		</Modal>
 		</>
 	);
 }
 
-class Item extends React.Component{
+class ItemButton extends React.Component{
 	constructor(props){
 		super(props);
+		this.state = {
+			itemCard: null
+		}
 		this.name = props.name;
+		this.description = props.description;
 		this.price = props.price;
+		this.showItemCard = this.showItemCard.bind(this);
+		this.hideItemCard = this.hideItemCard.bind(this);
+	}
+	showItemCard(){
+		const itemCard = (
+			<ItemCard 
+				image="https://via.placeholder.com/300x200"
+				title={this.name} 
+				description={this.description}
+			/>
+			)
+		this.setState({itemCard: itemCard});
+	}
+	hideItemCard(){
+		this.setState({itemCard: null});
 	}
 	render(){
 		return (
-			<button onClick={this.props.onClick}>{this.name} | Price: {this.price}G</button>
+			<>
+			<button 
+				onClick={this.props.onClick} 
+				onMouseOver={this.showItemCard} 
+				onMouseLeave={this.hideItemCard}
+			>
+				{this.name} | Price: {this.price}G
+				{this.state.itemCard}
+			</button>
+			</>
 		);
+	}
+}
+
+class ItemCard extends React.Component{
+	render(){
+		return(
+			<><Card id="ItemCard" style={{ width: '18rem', display: this.props.display }}>
+				<Card.Img id="ItemCardImage" src={this.props.image} />
+				<Card.Body>
+					<Card.Title>{this.props.title}</Card.Title>
+					<Card.Text>
+						{this.props.description}
+					</Card.Text>
+				</Card.Body>
+			</Card></>
+		)
+		
 	}
 }
 
@@ -50,11 +95,11 @@ class ShopManager extends React.Component{
 		this.state = {
 			cart: [],
 			itemsForSale: [
-				this.renderItem("Runeblade", 2600),
-				this.renderItem("Dark Heart Armor Set", 2800),
-				this.renderItem("Soul Calibur", 3000),
+				this.renderItem("Runeblade", "A blade from the Age of Runes.", 2600),
+				this.renderItem("Dark Heart Armor Set", "A set of armor from the darkest of days.", 3000),
 			],
 			gold: 10000,
+			itemCard: null
 		}
 		this.addItemToCart = this.addItemToCart.bind(this);
 		this.getItemByName = this.getItemByName.bind(this);
@@ -70,7 +115,6 @@ class ShopManager extends React.Component{
 		var item = this.getItemByName(itemName);
 		var cartItems = this.state.cart.concat(item);
 		this.setState({cart: cartItems});
-		console.log(this.state.cart);
 	}
 	itemInCart(itemName){
 		var item = this.getItemByName(itemName);
@@ -96,12 +140,15 @@ class ShopManager extends React.Component{
 	resetCart(){
 		this.setState({cart: []});
 	}
-	renderItem(itemName, price){
+	renderItem(itemName, desc, price){
 		return(
-			<Item
+			<ItemButton
 				name = {itemName}
 				price = {price}
+				description = {desc}
 				onClick = {() => this.addItemToCart(itemName)}
+				onMouseOver = {() => this.showItemCard()} 
+				onMouseOut = {() => this.hideItemCard()}
 			/>
 		)
 	}
@@ -109,16 +156,18 @@ class ShopManager extends React.Component{
 		const itemsForSale = this.state.itemsForSale.map(item => {
 			return <div key={item.props.name}>{item}</div>
 		})
+		var cartKey = 0;
 		const cart = this.state.cart.map(item =>
-			<div>{item.props.name}</div>	
+			<div key={cartKey++}>{item.props.name}</div>	
 		)
 		return (
 			<>
-				<SetGoldModal gold={this.state.gold} handleChange={this.handleGoldChange} />
-				{itemsForSale}
-				{cart}
-				<button onClick={this.purchaseItemsInCart}>Purchase</button>
-				<button onClick={this.resetCart}>Reset</button>
+			<SetGoldModal gold={this.state.gold} handleChange={this.handleGoldChange} />
+			{itemsForSale}
+			{cart}
+			<button onClick={this.purchaseItemsInCart}>Purchase</button>
+			<button onClick={this.resetCart}>Reset</button>
+			{this.state.itemCard}
 			</>
 		);
 	}
